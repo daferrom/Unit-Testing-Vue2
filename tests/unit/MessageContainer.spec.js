96c85053108ad2e48849d2c7b9a3dc83e0b65663
daferrom <daferrom@gmail.com>
2022-03-22 15:10:27 -0500

    l6 testing in progress

--- tests/unit/MessageContainer.spec.js
+++ tests/unit/MessageContainer.spec.js
@@ -3,6 +3,12 @@ import { mount } from '@vue/test-utils'
 
 describe('MessageContainer', () => {
   it('Wraps MessageDisplay component', () => {
-    const wrapper = mount(MessageContainer)
-  })
+    const wrapper = mount(MessageContainer, {
+      stubs: {
+        MessageDisplay: '<p v-else data-testid="message">{{ message.text }}</p>'
+      }
+    })
+    const message = wrapper.find('[data-testid="message"]').element.textContent
+    expect(message).toEqual('Hello from the db!')
+  })  
 })
